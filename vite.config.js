d4e2fa9f76a10bf702ce9ef07ae7bc9b2ac1ce83
ShivaniPednekar00 <shivanipednekar16@gmail.com>
2025-08-06 17:25:01 +0530

    AddedReact-router-dom

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/BookMyDoctor/", // this must match the GitHub repo name (case-sensitive)
   plugins: [react()],
-})
+});
